745bca49bcc2264fc83328aea037e68b053cc8ab
Sany <human.sany7@gmai.com>
2025-05-27 19:38:58 +0600

    jsx

--- explore-react-concept/src/App.jsx
+++ explore-react-concept/src/App.jsx
@@ -1,20 +1,25 @@
-import { useState } from "react";
-import reactLogo from "./assets/react.svg";
-import viteLogo from "/vite.svg";
-import "./App.css";
-
 function App() {
-  const [count, setCount] = useState(0);
-
   return (
     <>
       <h1>Vite + React</h1>
+      <Device name="Laptop" price="55"></Device>
+      <Device name="Phone" price=" 30"></Device>
+      <Device name="Watch" price="3"></Device>
       <Person></Person>
+      <Student grade = "7" score = "99"></Student>
       <Student></Student>
+      <Student></Student>
+      <Devoloper></Devoloper>
     </>
   );
 }
-
+function Device(props) {
+  return (
+    <h2>
+      This is {props.name} and price is {props.price}
+    </h2>
+  );
+}
 function Person() {
   // const age = 35;
   // const money = 60;
@@ -24,7 +29,7 @@ function Person() {
   };
 
   return (
-    <h3>
+    <h3 className="person">
       I am a person name {person.name} with age {person.age}
     </h3>
   );
@@ -32,10 +37,24 @@ function Person() {
 
 function Student() {
   return (
-    <div>
+    <div className="student">
       <h3>This is a student</h3>
       <p>Name: </p>
       <p>Age: 21</p>
+    </div>
+  );
+}
+
+function Devoloper() {
+  const devStyle = {
+    margin: "20px",
+    padding: "20px",
+    border: "2px solid purple",
+    borderRadius: "50px",
+  };
+  return (
+    <div style={devStyle}>
+      <p>Coding</p>
     </div>
   );
 }
