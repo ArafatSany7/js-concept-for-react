0ebfd0192523f692d7899823691555b2ba8a6eae
Sany <human.sany7@gmail.com>
2025-06-27 17:55:07 +0600

    extra

--- water-bottle/src/App.jsx
+++ water-bottle/src/App.jsx
@@ -1,10 +1,12 @@
 import "./App.css";
 import Bottles from "./components/Bottles/Bottles";
 import Header from "./components/Header/Header";
+import Nav from "./components/Navbar/Nav";
 
 function App() {
   return (
     <>
+      <Nav></Nav>
       <Header></Header>
       <Bottles></Bottles>
     </>
---
+++ water-bottle/src/components/Navbar/Nav.jsx
@@ -0,0 +1,16 @@
+import "./Nav.css";
+const Nav = () => {
+  return (
+    <div className="headnav">
+      <div className="font">
+        <p>Bottle</p>
+      </div>
+      <div className="menu">
+        <p>Menu</p>
+        <p>Cart</p>
+      </div>
+    </div>
+  );
+};
+
+export default Nav;
